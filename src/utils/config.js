64e7e7c77c51ea839c83a43ef0f36650187a864f
satrio1106 <satrio0226@gmail.com>
2023-11-10 13:10:39 +0700

    change api uri

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-// export const API_URI = 'https://wirabasa.com/v1/';
-export const API_URI = 'http://127.0.0.1:8000/v1/';
+export const API_URI = 'https://wirabasa.com/v1/';
+// export const API_URI = 'http://127.0.0.1:8000/v1/';
